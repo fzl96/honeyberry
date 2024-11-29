2182fa53a51a76941119331c6c9e009e5aa12c5e
fzl96 <fpsdragon333@gmail.com>
2024-11-29 13:27:32 +0700

    fix: Supress hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,7 +63,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
       <head />
       <body
         className={cn(
